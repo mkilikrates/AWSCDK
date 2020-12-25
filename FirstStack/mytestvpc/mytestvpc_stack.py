f15c0859eaeaf97073fb0d16d58dad907de58eb5
mkilikrates <7990460+mkilikrates@users.noreply.github.com>
2020-12-25 18:39:51 +0000

    ipv6 fixes + vpc flow logs

--- FirstStack/mytestvpc/mytestvpc_stack.py
+++ FirstStack/mytestvpc/mytestvpc_stack.py
@@ -7,11 +7,14 @@ from aws_cdk import (
     aws_iam as iam,
     aws_autoscaling as asg,
     aws_elasticloadbalancingv2 as elb,
+    aws_logs as log,
+    aws_cloudwatch as cw,
     core
 )
 logger = logging.getLogger(__name__)
 vcpcidr = "10.0.0.0/16"
 vpcname = "Myvpcpythontest"
+account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"])
 region = os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"])
 tagName = core.CfnTag(key="Name", value=vpcname)
 vpcenv = "production"
@@ -44,12 +47,25 @@ class MytestvpcStack(core.Stack):
                 )
             ]
         )
-        # IPv6 is currently not supported by CDK.
+        core.CfnOutput(
+            self,
+            "vpc-id",
+            value=myvpc.vpc_id
+        )
+         # IPv6 is currently not supported by CDK.
         # This is done manually now, based on:
         # https://gist.github.com/milesjordan/d86942718f8d4dc20f9f331913e7367a
         ipv6_block = ec2.CfnVPCCidrBlock(self, "Ipv6",
             vpc_id=myvpc.vpc_id,
             amazon_provided_ipv6_cidr_block=True
+        )
+        core.CfnOutput(
+            self,
+            "vpc-Cidrv6",
+            value=core.Fn.select(
+                0,
+                myvpc.vpc_ipv6_cidr_blocks
+            )
         )
         # We need to sniff out the InternetGateway the VPC is using, as we
         # need to assign this for IPv6 routing too.
@@ -115,6 +131,47 @@ class MytestvpcStack(core.Stack):
             )
             subnet.node.add_dependency(ipv6_block)
             i = i + 1
+        # Log Group for Flow Logs
+        myflowloggroup = log.LogGroup(
+            self,
+            "FlowLogsGroup",
+            retention=log.RetentionDays.ONE_WEEK
+        )
+        # Policy Document for Flow Logs
+        myflowpolicy = iam.PolicyStatement(
+            actions=[
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents"
+            ],
+            effect=iam.Effect.ALLOW,
+        )
+        # Add all resources to this policy document
+        myflowpolicy.add_all_resources()
+        # Iam Role
+        myflowlogrole = iam.Role(
+            self,
+            "FlowLogsRole",
+            assumed_by=iam.ServicePrincipal('vpc-flow-logs.amazonaws.com'),
+            description="Role for Delivery VPC Flow Logs",
+        )
+        # Attach policy to Iam Role
+        myflowlogrole.add_to_policy(myflowpolicy)
+        # Create VPC Flow Log for VPC
+        myvpcflowlog = ec2.CfnFlowLog(
+            self,
+            "VpcFlowLogs",
+            resource_id=myvpc.vpc_id,
+            resource_type='VPC',
+            traffic_type='ALL',
+            deliver_logs_permission_arn=myflowlogrole.role_arn,
+            log_destination_type='cloud-watch-logs',
+            log_group_name=myflowloggroup.log_group_name,
+            max_aggregation_interval=60,
+            log_format=format('${version} ${vpc-id} ${subnet-id} ${instance-id} ${interface-id} ${account-id} ${type} ${srcaddr} ${dstaddr} ${srcport} ${dstport} ${pkt-srcaddr} ${pkt-dstaddr} ${protocol} ${bytes} ${packets} ${start} ${end} ${action} ${tcp-flags} ${log-status}'),
+        )
         # get prefix list from file to allow traffic from the office
         with open('zonemap.cfg') as zonefile:
             zonemap = json.load(zonefile)
@@ -124,13 +181,22 @@ class MytestvpcStack(core.Stack):
             self,
             'MyBastionSG',
             allow_all_outbound=True,
-            vpc=myvpc
+            vpc=myvpc,
         )
         # add egress rule
-        # ignored by "allow_all_outbound but" although not create ipv6 rule
-        bastionsg.add_egress_rule(
-            ec2.Peer.ipv6("::/0"),
-            ec2.Port.all_traffic()
+        ec2.CfnSecurityGroupEgress(
+            self,
+            "BastionEgressAllIpv6",
+            ip_protocol="-1",
+            cidr_ipv6="::/0",
+            group_id=bastionsg.security_group_id
+        )
+        ec2.CfnSecurityGroupEgress(
+            self,
+            "BastionEgressAllIpv4",
+            ip_protocol="-1",
+            cidr_ip="0.0.0.0/0",
+            group_id=bastionsg.security_group_id
         )
         # add ingress rule
         bastionsg.add_ingress_rule(
@@ -166,7 +232,12 @@ class MytestvpcStack(core.Stack):
         bastion.instance.instance.add_property_override("KeyName", mykey)
         # enable ipv6 on this bastion
         bastion.instance.instance.add_property_override("Ipv6AddressCount", 1)
-        # set variables to be used
+        core.CfnOutput(
+            self,
+            "Instance-ipv4",
+            value=bastion.__getattribute__("instance_public_ip")
+        )
+        # set variables to be used on Auto Scale Group
         mykey = "mauranjo-" + region
         usrdatafile = 'simple_nginx'
         usrdata = open(usrdatafile + ".cfg", "r").read()
@@ -177,11 +248,22 @@ class MytestvpcStack(core.Stack):
             allow_all_outbound=True,
             vpc=myvpc
         )
+        # dependency to get IPV6 allow
+        asgsg.node.add_dependency(ipv6_block)
         # add egress rule
-        # ignored by "allow_all_outbound but" although not create ipv6 rule
-        asgsg.add_egress_rule(
-            ec2.Peer.ipv6("::/0"),
-            ec2.Port.all_traffic()
+        ec2.CfnSecurityGroupEgress(
+            self,
+            "ASGEgressAllIpv6",
+            ip_protocol="-1",
+            cidr_ipv6="::/0",
+            group_id=asgsg.security_group_id
+        )
+        ec2.CfnSecurityGroupEgress(
+            self,
+            "ASGEgressAllIpv4",
+            ip_protocol="-1",
+            cidr_ip="0.0.0.0/0",
+            group_id=asgsg.security_group_id
         )
         # add ingress rule
         asgsg.add_ingress_rule(
@@ -222,21 +304,35 @@ class MytestvpcStack(core.Stack):
             "myALB",
             vpc=myvpc,
             internet_facing=True,
+            ip_address_type=elb.IpAddressType("DUAL_STACK"),
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-            security_group=bastionsg
+            #security_group=bastionsg
+        )
+        core.CfnOutput(
+            self,
+            "ALB DNS",
+            value=myalb.load_balancer_dns_name
         )
         # create listener
         mylistnr = myalb.add_listener(
             "Listener_http",
             port=80,
-            protocol=elb.ApplicationProtocol.HTTP
+            protocol=elb.ApplicationProtocol.HTTP,
         )
         # define asg
-        mylistnr.connections.allow_from(myalb,ec2.Port.tcp(80))
+        mylistnr.connections.allow_from(
+            myalb,
+            ec2.Port.tcp(80)
+        )
+        mylistnr.connections.allow_default_port_from(
+            other=ec2.Peer.any_ipv6(),
+            description="Allow from anyone on port 80"
+        )
         # associate listener to asg
         mylistnr.add_targets(
             "MyFleet from myasg",
             port=80,
             targets=[myasg]
         )
+        
 
