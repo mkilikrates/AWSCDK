f257e6157352093cbef4733ab64697f80253f1b3
EC2 Default User <ec2-user@ip-172-31-28-7.eu-west-1.compute.internal>
2021-06-08 16:06:11 +0000

    fix

--- multistack/multistack/decodevpn.py
+++ multistack/multistack/decodevpn.py
@@ -35,7 +35,7 @@ with open(resconf) as resfile:
 
 class S2SVPNS3(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, route, res, vpnid = str, vpc = ec2.Vpc, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, route, funct, res, vpnid = str, vpc = ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
@@ -45,61 +45,63 @@ class S2SVPNS3(core.Stack):
         self.route = route
         res = res
         self.bucketname = resmap['Mappings']['Resources'][res]['S3']
-        # create Police for lambda function
-        self.mylambdapolicy = iam.PolicyStatement(
-            actions=[
-                "ec2:DescribeVpnConnections",
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:DescribeLogGroups",
-                "logs:DescribeLogStreams",
-                "logs:PutLogEvents"
-            ],
-            resources=["*"],
-            effect=iam.Effect.ALLOW
-        )
-        self.mylambdaS3policy = iam.PolicyStatement(
-            actions=[
-                "s3:GetObject",
-                "s3:ListBucket",
-                "s3:DeleteObject",
-                "s3:PutObject"
-            ],
-            resources=[
-                f"arn:aws:s3:::{self.bucketname}",
-                f"arn:aws:s3:::{self.bucketname}/vpn/*"
-            ],
-            effect=iam.Effect.ALLOW
-        )
-        self.mylambdarole = iam.Role(
-            self,
-            "LambdaRole",
-            assumed_by=iam.ServicePrincipal(
-                'lambda.amazonaws.com'
-            ),
-            description=(
-                'Role for Lambda to write vpn config files on s3 bucket as Custom Resources in CloudFormation'
+        if funct =='':
+            # create Police for lambda function
+            self.mylambdapolicy = iam.PolicyStatement(
+                actions=[
+                    "ec2:DescribeVpnConnections",
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:DescribeLogGroups",
+                    "logs:DescribeLogStreams",
+                    "logs:PutLogEvents"
+                ],
+                resources=["*"],
+                effect=iam.Effect.ALLOW
             )
-        )
-        self.mylambdarole.add_to_policy(self.mylambdapolicy)
-        self.mylambdarole.add_to_policy(self.mylambdaS3policy)
-        # Create Lambda Function
-        self.mylambda = lpython.PythonFunction(
-            self,
-            f"{construct_id}:Lambda",
-            handler="lambda_handler",
-            timeout=core.Duration.seconds(90),
-            runtime=lambda_.Runtime.PYTHON_3_8,
-            description="Lambda to write vpn config files on s3 bucket  as Custom Resources in CloudFormation",
-            entry="lambda/DecoderVPN/",
-            role=(self.mylambdarole),
-            log_retention=log.RetentionDays.ONE_WEEK
-        )
+            self.mylambdaS3policy = iam.PolicyStatement(
+                actions=[
+                    "s3:GetObject",
+                    "s3:ListBucket",
+                    "s3:DeleteObject",
+                    "s3:PutObject"
+                ],
+                resources=[
+                    f"arn:aws:s3:::{self.bucketname}",
+                    f"arn:aws:s3:::{self.bucketname}/vpn/*"
+                ],
+                effect=iam.Effect.ALLOW
+            )
+            self.mylambdarole = iam.Role(
+                self,
+                "LambdaRole",
+                assumed_by=iam.ServicePrincipal(
+                    'lambda.amazonaws.com'
+                ),
+                description=(
+                    'Role for Lambda to write vpn config files on s3 bucket as Custom Resources in CloudFormation'
+                )
+            )
+            self.mylambdarole.add_to_policy(self.mylambdapolicy)
+            self.mylambdarole.add_to_policy(self.mylambdaS3policy)
+            # Create Lambda Function
+            self.mylambda = lpython.PythonFunction(
+                self,
+                f"{construct_id}:Lambda",
+                handler="lambda_handler",
+                timeout=core.Duration.seconds(90),
+                runtime=lambda_.Runtime.PYTHON_3_8,
+                description="Lambda to write vpn config files on s3 bucket  as Custom Resources in CloudFormation",
+                entry="lambda/DecoderVPN/",
+                role=(self.mylambdarole),
+                log_retention=log.RetentionDays.ONE_WEEK
+            )
+            funct = self.mylambda.function_arn
         
         self.mycustomresource = core.CustomResource(
             self,
             f"{construct_id}:CustomResource",
-            service_token=self.mylambda.function_arn,
+            service_token=funct,
             properties=[
                 {
                     "VPN" : self.vpnid,
@@ -111,9 +113,9 @@ class S2SVPNS3(core.Stack):
                 }
             ]
         )
-        core.CfnOutput(
+        self.funct = core.CfnOutput(
             self,
             f"{construct_id}:LambdaArn",
-            value=self.mylambda.function_arn,
+            value=funct,
             export_name=f"{construct_id}:LambdaArn"
         )
